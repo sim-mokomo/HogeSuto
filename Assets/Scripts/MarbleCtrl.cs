c16580f65c5945fba7dec53dc6208d479ddf4353
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2018-08-22 03:08:46 +0900

    フリックの具合を見て矢印のスケールを調整するように

--- Assets/Scripts/MarbleCtrl.cs
+++ Assets/Scripts/MarbleCtrl.cs
@@ -7,6 +7,12 @@ public class MarbleCtrl : MonoBehaviour
 	private Rigidbody2D _rigidbody2D;
 	[SerializeField] private float _flickPower;
 	[Range(0.0f,1.0f)] [SerializeField] private float _fliction;
+
+	[SerializeField] private GameObject flickArrowRoot;
+	[SerializeField] private GameObject flickArrow;
+	[SerializeField] private float flickScalingMaxBounds;
+	[SerializeField] private float flickMinSize;
+	[SerializeField] private float flickMaxSize;
 	
 	public void Init()
 	{
@@ -14,7 +20,28 @@ public class MarbleCtrl : MonoBehaviour
 		FlickListener.OnEndFlick += flickData =>
 		{
 			Debug.Log(flickData.ToString());
-			_rigidbody2D.velocity = flickData.FlickDirection * _flickPower;
+			_rigidbody2D.velocity = flickData.FlickDirection * -_flickPower;
+			flickArrow.transform.localScale = Vector3.one;
+		};
+
+		FlickListener.OnFlicking += flickingPos =>
+		{
+			Vector3 diff = flickingPos - transform.position;
+			float angle = Mathf.Atan2(diff.y,diff.x) * Mathf.Rad2Deg;
+			angle += 90.0f;
+			flickArrowRoot.transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, angle));
+
+			float distToMouse = (flickingPos - transform.position).sqrMagnitude;
+			distToMouse = distToMouse - 100.0f;
+			float distToMouseRatio = distToMouse / flickScalingMaxBounds;
+			
+			Debug.Log($"dist to mouse {distToMouse}");
+			Debug.Log($"dist to mouse distance ratio {distToMouseRatio}");
+			
+			float scalingSize = Mathf.Lerp(flickMinSize, flickMaxSize, distToMouseRatio);
+			Vector3 scale = Vector3.one;
+			scale.y = scalingSize;
+			flickArrow.transform.localScale = scale;
 		};
 	}
 
