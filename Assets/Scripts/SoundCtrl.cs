25cdfd4a1a6773849e3f0dc799851c195de78cba
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2018-08-22 08:18:04 +0900

    SE再生用のSoundCtrl実装

---
+++ Assets/Scripts/SoundCtrl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundCtrl : MonoBehaviour {
+
+    private static AudioSource audioSource;
+    public static Action OnPlaySound;
+    public static Action OnEndSound;
+    
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        OnPlaySound += () => { StartCoroutine(CheckEnd(onEnd: OnEndSound)); };
+    }
+
+    public static void PlayOneShot(AudioClip audioClip,Action onEnd=null)
+    {
+        audioSource.PlayOneShot(audioClip);
+        OnPlaySound?.Invoke();
+    }
+
+    private static IEnumerator CheckEnd(Action onEnd)
+    {
+        var frameWait = new WaitForEndOfFrame();
+        while(true)
+        {
+            yield return frameWait;
+            if(audioSource.isPlaying == false)
+            {
+                onEnd?.Invoke();
+                break;
+            }
+        }
+    }
+
+}
