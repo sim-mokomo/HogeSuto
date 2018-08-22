c5a500367153379181bebefd9be3ae4c969dfa4b
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2018-08-22 12:56:57 +0900

    ぶつかることで何かしらの効力が出る物を扱うインターフェイス作成

---
+++ Assets/Scripts/I_StrikeDetectable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface I_StrikeDetectable  {
+
+	bool IsDetectable { get; }
+	void StrikeDetect();
+
+}
