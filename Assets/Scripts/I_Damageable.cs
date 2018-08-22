45c7c803dddc6f91e7f36d55c6b4060761955728
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2018-08-22 21:23:35 +0900

    ダメージを与えることが可能なインターフェイス追加

---
+++ Assets/Scripts/I_Damageable.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class DamageInfo
+{
+    
+    public DamageInfo(float damageValue, GameObject attacker)
+    {
+        this.DamageValue = damageValue;
+        this.Attacker = attacker;
+    }
+
+    public float DamageValue { get; private set; }
+    public GameObject Attacker { get; private set; }
+    
+}
+
+public interface I_Damageable
+{
+    bool IsDamageable { get; }
+    void ApplyDamage(DamageInfo damageInfo);
+}
